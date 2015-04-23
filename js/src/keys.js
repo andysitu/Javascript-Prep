77072137f1e65a7cf098694fda8340431cae9474
Andy <and.situ@gmail.com>
2015-04-22 21:00:28 -0700

    Added keys
    * Added _.keys and tests case by iterating with for loop.

---
+++ js/src/keys.js
@@ -0,0 +1,17 @@
+/** Retrieve all the names of the object's own enumerable properties.
+ * 
+ * _.keys({one: 1, two: 2, three: 3});
+ * 
+ * Example:
+ * => ["one", "two", "three"]
+ *
+*/
+
+_.keys = function(obj) {
+	var list = [];
+	for (var par in obj) {
+		list.push(par);
+	}
+	console.log(list);
+	return list;
+}
---
+++ js/test/keys.js
@@ -0,0 +1,18 @@
+QUnit.test("keys Test", function( assert ) {
+
+	var list1 = _.keys({one: 1, two: 2, three: 3});
+	var list2 = _.keys({1: "fire", 2: "sto"});
+	var test1 = ["one", "two", "three"];
+	var test2 = ["1", "2"];
+
+	console.log(list1 === test1);
+
+	for (var i = 0; i <= 3; i++) {
+		assert.equal(list1[i], test1[i], 
+			"keys testing with Object {one: 1, two: 2, three: 3}");
+	}
+	for (var i = 0; i <= 2; i++) {
+		assert.equal(list2[i], test2[i], 
+			"keys testing with Object {1: \"fire\", 2: \"sto\"}");
+	}
+});
