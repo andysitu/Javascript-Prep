9640cf53200fc891f9e4600f928ff3f2bdfc1428
Andy <and.situ@gmail.com>
2015-05-02 14:04:56 -0700

    Slight change of value var in find.js
    * moved it to a higher scope and deleted the else clauses that include
    it as a result of this.

--- js/src/find.js
+++ js/src/find.js
@@ -1,22 +1,17 @@
 _.find = function(list, predicate, context) {
+	var value = undefined;
 	if (Object.prototype.toString.call(list) == "[object Array]") {
-		var value = 0;
 		for (var i = 0; i < list.length; i++) {
 			if (predicate.call(context, list[i])) {
 				value = list[i];
 				break;
-			} else {
-				value = undefined;
 			}
 		}
 	} else {
-		var value = 0;
 		for (var key in list) {
 			if (predicate.call(context, list[key])) {
 				value = list[key];
 				break;
-			} else {
-				value = undefined;
 			}
 		}
 	}
